8a172e0ad2701e0a3cec0bd72e693b444cd860b1
matias0073 <64817445+matias0073@users.noreply.github.com>
2024-06-27 00:44:35 -0300

    js.js

--- js.js
+++ js.js
@@ -9,25 +9,24 @@ function addTask() {
 
   var li = document.createElement("li");
 
-  // Crear checkbox para marcar como completada
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.onclick = function() {
       if (checkbox.checked) {
           li.style.textDecoration = "line-through";
       } else {
-          li.style.textDecoration = "none"; // Quitar el tachado si no está completado
+          li.style.textDecoration = "none";
       }
   };
 
   li.appendChild(checkbox);
 
-  // Agregar texto de la tarea
+ 
   var taskText = document.createElement("span");
   taskText.textContent = taskInput.value;
   li.appendChild(taskText);
 
-  // Botón de eliminar tarea
+ 
   var deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
   deleteButton.onclick = function() {
@@ -36,9 +35,7 @@ function addTask() {
 
   li.appendChild(deleteButton);
 
-  // Agregar la tarea a la lista
   taskList.appendChild(li);
 
-  // Limpiar el input después de agregar la tarea
   taskInput.value = "";
 }
